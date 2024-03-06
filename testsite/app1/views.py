e9bf32af8c92065695c1ff5100d53ecc2b096afc
Ruslan Fedorov <ruslan.fedorov@icl-services.com>
2024-03-06 14:29:57 +0300

    1

--- testsite/app1/views.py
+++ testsite/app1/views.py
@@ -1,8 +1,16 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 import ansible_runner
 
 def index(request):
-    a = ansible_runner.run(private_data_dir="/home/localadmin/ald-servers-install", 
-                           playbook="playbook.yaml", 
-                           quiet=True)
-    return HttpResponse(a.status)
+    if request.method == 'POST':
+        with open("/home/localadmin/ald-servers-install/inventory/hosts", mode="w", encoding="utf-8") as out:
+            out.write(render(request, 'hosts.j2', {}))
+        # a = ansible_runner.run(
+        #     private_data_dir="/home/localadmin/ald-servers-install", 
+        #     playbook="playbook.yaml", 
+        #     quiet=True,
+        # )
+        # return render(request, 'finish.html', { 'status': a.status, })
+        return render(request, 'finish.html', { 'status': "ok!", })
+    return render(request, 'index.html', {})
