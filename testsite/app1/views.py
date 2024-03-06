ce983641a722a1a21882c698a14435333f370408
Ruslan Fedorov <ruslan.fedorov@icl-services.com>
2024-03-06 15:19:56 +0300

    2

--- testsite/app1/views.py
+++ testsite/app1/views.py
@@ -7,11 +7,10 @@ def index(request):
     if request.method == 'POST':
         with open("/home/localadmin/ALDPro-deploy/inventory/hosts", mode="w", encoding="utf-8") as out:
             out.write(render_to_string('hosts.j2', { "request" : request }))
-        # a = ansible_runner.run(
-        #     private_data_dir="/home/localadmin/ALDPro-deploy", 
-        #     playbook="playbook.yaml", 
-        #     quiet=True,
-        # )
-        # return render(request, 'finish.html', { 'status': a.status, })
-        return render(request, 'finish.html', { 'status': "ok!", })
+        a = ansible_runner.run(
+            private_data_dir="/home/localadmin/ALDPro-deploy", 
+            playbook="playbook.yaml", 
+            quiet=True,
+        )
+        return render(request, 'finish.html', { 'status': a.status, })
     return render(request, 'index.html', {})
